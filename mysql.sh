c7a62db5fde49440e760c813cecd754b45c37c6d
raj <raj.pradeep676@gmail.com>
2024-04-20 16:39:05 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=($date +%F-H%-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
@@ -14,48 +14,41 @@ read -s mysql_root_password
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo -e "$2... $R FAILURE $N"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo  -e "$2...$G SUCCESS $N "
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
 if [ $USERID -ne 0 ]
 then
-    echo "please run the scripts with roots access"
-    exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo " your are super user"
+    echo "You are super user."
 fi
 
 
-
 dnf install mysql-server -y &>>$LOGFILE
-
-VALIDATE $? "installing the mysql"
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "enabling mysql"
+VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "starting mysql"
+VALIDATE $? "Starting MySQL Server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+ VALIDATE $? "Setting up root password"
 
-mysql -h db.pradeep17.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-
+ #Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
-then 
-     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-     VALIDATE $? "MySQL Root password Setup"
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
 else
-    echo -e "my root password is already setup $Y Skipping $N "
- fi
-
-
-
-
-
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
 
