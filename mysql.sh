5be6ce099ad4977760fa35ce1d7b8b20c4c10df2
raj <raj.pradeep676@gmail.com>
2024-04-20 15:37:30 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=($date +%F-H%-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-        echo  "$2... FAILURE "
+        echo -e "$2... $R FAILURE $N"
         exit 1
     else
-        echo  "$2...$ SUCCESS "
+        echo  -e "$2...$G SUCCESS $N "
     fi
 }
-
-
-
 
 if [ $USERID -ne 0 ]
 then
@@ -25,7 +32,7 @@ fi
 
 
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGFILE
 
 VALIDATE $? "installing the mysql"
 
