3868c76747d5b450f508e89b3826bc208b369bd7
raj <raj.pradeep676@gmail.com>
2024-04-20 15:11:30 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]
+then
+    echo "please run the scripts with roots access"
+    exit 1
+else
+    echo " your are super user"
+fi
+
+dnf install mysql-server -y
