706e0ecd2fce67e75342ef0fa4b3180f6c5453a4
raj <raj.pradeep676@gmail.com>
2024-04-21 08:57:07 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -67,7 +67,26 @@ VALIDATE $? "Extracted backend code"
 npm install
 VALIDATE $? "Installing nodejs dependencies"
 
+ cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service
 
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
 
 
 
