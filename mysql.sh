994501d10c6fe747c6f58cca67cfb98e8277b4ad
raj <raj.pradeep676@gmail.com>
2024-04-21 07:12:12 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+
+if ( $1 -ne 0 )
+then
+echo -e " $2... installation is $R fails $N"
+exit 1
+else
+echo -e " $2...installation is $G SUccess $N"
+fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -10,6 +28,16 @@ echo "your are super user"
 
 fi 
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "installing the mysql"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "enabling the mysql"
+
+systemctl start mysqld
+VALIDATE $? "starting the mysql"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+ VALIDATE $? "Setting up root password"
 
 
