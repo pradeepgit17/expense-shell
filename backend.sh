6babbbb2554569fb07917bfa954ea93cf18a3239
raj <raj.pradeep676@gmail.com>
2024-04-21 08:22:06 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+USERID=$(id -u)
+
+if [ USERID -ne 0 ]
+then
+    echo " please run the scripts with root user"
+    exit 1
+else 
+    echo  " your are super user"
+
+f1
+
+dnf module disable nodejs -y
