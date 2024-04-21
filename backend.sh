c2a033244fa9abb9a3dc477fc8acbe2c3abe8fbc
raj <raj.pradeep676@gmail.com>
2024-04-21 08:45:07 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -38,8 +38,19 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-useradd expense
-VALIDATE $? "creating expense user"
+#useradd expense
+#VALIDATE $? "creating expense user"
+
+id expense
+
+if [ $? -ne 0 ]
+then 
+   useradd expense
+   VALIDATE $? "creating expense user"
+else
+
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
 
 
 
