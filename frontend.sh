6fe93a2a772f5e6e8d0b0d8634c7da1b84eff3fd
raj <raj.pradeep676@gmail.com>
2024-04-21 10:10:56 +0530

    frountend

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Please enter DB password:"
+read  mysql_root_password
+
+#ExpenseApp@1
+VALIDATE(){
+
+   if [ $1 -ne 0 ]
+then
+   echo  -e" $2... installation is $R fails $N"
+   exit 1
+else
+    echo -e " $2... installation is $G SUccess $N"
+fi
+}
+
 
 if [ $USERID -ne 0 ]
 then
@@ -10,4 +33,28 @@ else
   echo "your are super user"
 fi
 
-dnf install nginx -y 
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing existing content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code"
+
+p /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense conf"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
+
