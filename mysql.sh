1d2f0b1ef1d2bcb45ec68cd4a1dd9aeb01c35049
raj <raj.pradeep676@gmail.com>
2024-04-21 08:16:34 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -44,10 +44,10 @@ VALIDATE $? "starting the mysql"
  #mysql_secure_installation --set-root-pass ExpenseApp@1
  #VALIDATE $? "Setting up root password"
 
-mysql -h 172.31.20.179 -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h 172.31.20.179 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${ExpenseApp@1} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
