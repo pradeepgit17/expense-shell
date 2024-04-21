e9d633a9176c98bba29b48bc2870572c5ca62300
raj <raj.pradeep676@gmail.com>
2024-04-21 08:36:54 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+
+if [ $1 -ne 0]
+then
+   echo  -e" $2... installation is $R fails $N"
+   exit 1
+else
+    echo -e " $2... installation is $G SUccess $N"
+fi
+}
 
 if [ $USERID -ne 0 ]
 then
@@ -11,5 +29,20 @@ else
 
 fi
 
-dnf install nodejs -y
+dnf module disable nodejs -y
+VALIDATE $? " disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+useradd expense
+VALIDATE $? "creating expense user"
+
+
+
+
+
 
